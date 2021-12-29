cf4829476097a1f167d2d96912b00486c76ccdeb
Aniruddh Sharma <91824428+an1rudh-s@users.noreply.github.com>
2021-12-29 23:51:26 +0530

    Add files via upload

---
+++ precache-manifest.cbc517c442da1000f463d52374971ca8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "220b5ac769902cd382c6",
+    "url": "/robofriends/static/css/main.b2355b32.chunk.css"
+  },
+  {
+    "revision": "220b5ac769902cd382c6",
+    "url": "/robofriends/static/js/main.220b5ac7.chunk.js"
+  },
+  {
+    "revision": "0bf54a1201dab9a9b136",
+    "url": "/robofriends/static/js/1.0bf54a12.chunk.js"
+  },
+  {
+    "revision": "0b481d9a0de5aa8a7929",
+    "url": "/robofriends/static/css/2.dec7a759.chunk.css"
+  },
+  {
+    "revision": "0b481d9a0de5aa8a7929",
+    "url": "/robofriends/static/js/2.0b481d9a.chunk.js"
+  },
+  {
+    "revision": "d9078623073d367ccac9",
+    "url": "/robofriends/static/js/runtime~main.d9078623.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "8fc9b55433c52a7f58ba2c0de968483c",
+    "url": "/robofriends/index.html"
+  }
+];
